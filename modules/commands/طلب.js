5956d3e5df8eb59685a88b9cc1abc71714aa6c44
hwmatwkytw-rgb <hwmatwkytw@gmail.com>
2026-06-23 17:59:26 +0200

    Update doc_id for add and del commands

--- modules/commands/طلب.js
+++ modules/commands/طلب.js
@@ -34,11 +34,11 @@ module.exports.handleReply = async ({ handleReply, event, api }) => {
 
   if (args[0] == "add") {
     form.fb_api_req_friendly_name = "FriendingCometFriendRequestConfirmMutation";
-    form.doc_id = "3147613905362928";
+    form.doc_id = "100081948980908";
   }
   else if (args[0] == "del") {
     form.fb_api_req_friendly_name = "FriendingCometFriendRequestDeleteMutation";
-    form.doc_id = "4108254489275063";
+    form.doc_id = "100081948980908";
   }
   else return api.sendMessage("Please select <add | del > <so thu tu | or \"all\">", event.threadID, event.messageID);
   let targetIDs = args.slice(1);
